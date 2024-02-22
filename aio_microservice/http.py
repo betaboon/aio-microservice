bd0580808a37e001ba0021cee0124d869512a19e
betaboon <betaboon@0x80.ninja>
2024-02-22 14:48:57 +0100

    feat(http): add support for litestart controllers

--- aio_microservice/http.py
+++ aio_microservice/http.py
@@ -1,9 +1,10 @@
-from litestar import delete, get, patch, post, put, status_codes
+from litestar import Controller, delete, get, patch, post, put, status_codes
 
 from aio_microservice.core.service import HttpSettings
 from aio_microservice.core.testing import TestHttpClient
 
 __all__ = [
+    "Controller",
     "HttpSettings",
     "TestHttpClient",
     "delete",
