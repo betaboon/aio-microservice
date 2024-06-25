dc4fd87b02c1894cbb70fbeb696bc7e39afaa895
Riccardo Padoan <riccardo.padoan@exb.de>
2024-06-25 18:33:40 +0100

    test(scheduler): add test for scheduled task called before app is ready

--- tests/scheduler/test_scheduler.py
+++ tests/scheduler/test_scheduler.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 import asyncio
 from typing import TYPE_CHECKING
 
-from aio_microservice import Service, ServiceSettings, scheduler
+import pytest
+
+from aio_microservice import Service, ServiceSettings, readiness_probe, scheduler
 from aio_microservice.http import TestHttpClient
 from aio_microservice.scheduler import SchedulerExtension
 
@@ -55,3 +57,37 @@ def test_scheduler_crontab() -> None:
 
     jobs = service.scheduler.scheduler.get_jobs()
     assert len(jobs) == 1
+
+
+@pytest.mark.xfail(reason="Needs custom service lifecycle hook")
+async def test_scheduler_does_not_run_before_service_ready(mocker: MockerFixture) -> None:
+    class TestService(Service[ServiceSettings], SchedulerExtension):
+        def __init__(self, settings: ServiceSettings | None = None) -> None:
+            super().__init__(settings=settings)
+            self.is_ready = False
+            self.interval_method_stub = mocker.stub()
+
+        @readiness_probe
+        async def readiness_probe(self) -> bool:
+            return self.is_ready
+
+        @scheduler.interval(seconds=1)
+        async def do_every_second(self) -> None:
+            self.interval_method_stub(self.is_ready)
+
+    service = TestService()
+    async with TestHttpClient(service=service):
+        assert service.is_ready is False
+
+        # give enough time to "potentially" execute the interval twice
+        await asyncio.sleep(2)
+
+        assert service.interval_method_stub.call_count == 0
+
+        service.is_ready = True
+
+        # give enough time to "potentially" execute the interval twice
+        await asyncio.sleep(2)
+
+        assert service.interval_method_stub.call_count > 0
+        assert service.interval_method_stub.call_args.args[0] is True
